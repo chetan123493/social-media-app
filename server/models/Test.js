1290337a6dd4a14b8157014e012943eb61026b37
chetan <chetanyaadepu@gmail.comgit config --global user.email chetanyaadepu@gmail.com>
2025-05-06 03:40:59 +0530

    Clean re-commit without LFS

--- server/models/Test.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// Schema for test data
-const testSchema = new mongoose.Schema({
-  name: String
-});
-
-module.exports = mongoose.model('Test', testSchema);
--- server/routers/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authenticateToken = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/', authenticateToken, (req, res) => {
-  res.json({ message: 'Token is valid', user: req.user });
-});
-
-module.exports = router;
